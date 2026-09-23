075e8e11e845ba7d6fefc9a204b15a1e5bc2655f
jannatdev <jannat2557@gmail.com>
2026-09-23 15:49:08 +0600

    Some file is updated

--- src/lib/appFetch.ts
+++ src/lib/appFetch.ts
@@ -1,6 +1,9 @@
+
+import data from "@/data/data.json"
+
 export const getApps=async()=>{
-    const res = await fetch('http://localhost:3000/data.json');
-    const data = await res.json();
+    
+    
     return data;
 
  }
